eecf3ef99c7953757d228ae6c27456fdc49e8493
PawelYak <stojakpawel@gmail.com>
2021-03-09 07:36:46 +0100

    Removed comments

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include <algorithm>
-#include <ios>
+
 #include <iostream>
 #include <string>
 
 bool isPalindrom(const std::string &text) {
-  std::cout << "before : " << text << '\n';
+
   std::string t2{text};
   std::reverse_copy(text.begin(), text.end(), t2.begin());
-  std::cout << "After: " << t2 << '\n';
   return text == t2;
 }
 int main() {
@@ -16,6 +15,5 @@ int main() {
   std::cin >> inputText;
 
   std::cout << isPalindrom(inputText) << '\n';
-
   return 0;
 }
